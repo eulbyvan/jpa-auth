20cb12ab07bbbbc7bfd211347a44b8fc86e82486
eulbyvan <work.issdq@gmail.com>
2022-11-21 14:02:55 +0700

    added User model

---
+++ src/main/java/com/enigmacamp/model/User.java
@@ -0,0 +1,47 @@
+package com.enigmacamp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "is_locked")
+    private Boolean isLocked;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(Boolean locked) {
+        isLocked = locked;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isLocked=" + isLocked +
+                '}';
+    }
+}
