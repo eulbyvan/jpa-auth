c255386bcac488cb79ce7e1149f2183252e723bc
eulbyvan <work.issdq@gmail.com>
2022-11-21 14:21:08 +0700

    updated db info

--- src/main/java/com/enigmacamp/App.java
+++ src/main/java/com/enigmacamp/App.java
@@ -1,11 +1,21 @@
 package com.enigmacamp;
 
 import com.enigmacamp.console.MasterMenu;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+            EntityManager em = emf.createEntityManager();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         try {
             MasterMenu.run();
         } catch (SQLException e) {
